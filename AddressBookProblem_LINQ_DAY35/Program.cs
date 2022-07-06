b5c2683649740843d0b08d198311f2397d4804d5
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-07 01:47:07 +0530

    [Tushar Vidwans] Inserting data into addressBookTable table

--- AddressBookProblem_LINQ_DAY35/Program.cs
+++ AddressBookProblem_LINQ_DAY35/Program.cs
@@ -61,6 +61,16 @@ namespace AddressBookProblem_LINQ_DAY35
             PrimaryKeyColumns[0] = addressBookTable.Columns["firstName"];
             PrimaryKeyColumns[1] = addressBookTable.Columns["phoneNumber"];
             addressBookTable.PrimaryKey = PrimaryKeyColumns;
+
+
+
+            //Inserting data into columns into datatable UC3
+            addressBookTable.Rows.Add("Tushar", "Vidwans", "Malkapur", "Akola", "Maharashtra", 444004, 9879847453, "tushar.vidwans");
+            addressBookTable.Rows.Add("Apoorva", "Patil", "Ambika Nagar", "hisar", "Telangana", 125433, 8585858585, "appoo.p");
+            addressBookTable.Rows.Add("Amit", "Kumar", "Andheri", "Mumbai", "Maharashtra", 125445, 7575757575, "amit.kumar");
+            addressBookTable.Rows.Add("Kamalakar", "Singh", "Thanesar", "Kurukshetra", "Haryana", 136119, 9595959595, "kamalakar.singh");
+            addressBookTable.Rows.Add("Praveen", "Kumar", "rohini", "Delhi", "Delhi", 435121, 7897897898, "praveen.kumar");
+            addressBookTable.Rows.Add("Apoorva", "Singh", "Andheri", "Mumbai", "Maharashtra", 125445, 8598598599, "apporva.singh");
         }
     }
 }
