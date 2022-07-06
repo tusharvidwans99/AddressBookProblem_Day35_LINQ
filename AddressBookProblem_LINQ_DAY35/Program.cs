256d319a3146d48d44d9bdcf1028db19aa7275bf
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-07 01:34:04 +0530

    [Tushar Vidwans] Creating addressBookTable table in

--- AddressBookProblem_LINQ_DAY35/Program.cs
+++ AddressBookProblem_LINQ_DAY35/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace AddressBookProblem_LINQ_DAY35
+﻿using System.Data;
+
+namespace AddressBookProblem_LINQ_DAY35
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Problem for LINQ");
+
+            //Creating DataTable for addressbook problem UC1
+            DataTable addressBookTable = new DataTable();
         }
     }
 }
